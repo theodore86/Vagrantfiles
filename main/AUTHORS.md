df88be992b1ee26e7f152b4127f6652d36ad7f5b
Theogeo <tgeorgomanolis@gmail.com>
2019-09-04 22:27:38 +0300

    Restructure repository to have only sample Vagrantfiles

--- main/AUTHORS.md
+++
@@ -1,3 +0,0 @@
-# Contributors
-
-* Theodore Georgomanolis <tgeorgomanolis@gmail.com>
--- main/provisioners/shell/install-tox.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-echo "INFO>>> Installing Tox automation project"
-
-python -m pip --version
-python -m pip install --user tox
-python -m tox --version
